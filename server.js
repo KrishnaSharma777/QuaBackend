4078009aaa90f7461e235640ec4d89dc429c844c
KrishnaQualoam <krishnasharma023456@gmail.com>
2025-05-30 10:28:00 +0530

    last

--- server.js
+++ server.js
@@ -22,3 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
   })
   .catch(err => console.error(err));
+  console.log();
+  
